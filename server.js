61479787f2dee911107a17429683bfe3a7d3c802
aqtagon <ahmed.danawi@gmail.com>
2023-12-29 21:19:14 -0800

    Made adjustments to routes

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -7,16 +11,23 @@ app.use(express.urlencoded({extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.log('Server listening on port ${PORT}');
-});
-
-const path = require('path');
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}');
 });
